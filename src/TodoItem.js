1cd5026ffa2d974e6a9e825e158b043918385ef1
zeroone001 <ilylys@163.com>
2019-06-01 15:45:51 +0800

    update

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'; 
 
 class TodoItem extends Component {
     constructor (props) {
@@ -21,5 +22,11 @@ class TodoItem extends Component {
     }
 
 }
+
+TodoItem.propTypes = {
+    content: PropTypes.string,
+    handleDelete: PropTypes.func,
+    index: PropTypes.number
+};
 
 export default TodoItem;
