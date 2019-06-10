97e2b46749855e04c3cf6e7002b488253e49739b
zeroone001 <ilylys@163.com>
2019-06-11 07:31:23 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 import './index.css';
 // import App from './App';
 import TodoList from './TodoList.js';
 
 ReactDOM.render(
-    <Provider store="asd">
-        <TodoList />
-    </Provider>, 
+    // <Provider store="asd">
+        <TodoList />,
+    // </Provider>, 
     document.getElementById('root')
 );
 
